bdba3a59d9e115f5e973a70f0bcef01e5f6a5bc4
John P. Bloch <john.bloch@10up.com>
2014-07-26 15:00:56 -0600

    Add bare function lib wrapper abstract class and empty test for it

---
+++ src/Util/FunctionLibWrapper.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace League\Bumble\Util;
+
+abstract class FunctionLibWrapper
+{
+
+    /**
+     * A common prefix at the beginning of all function calls.
+     * @var string
+     */
+    protected $prefix = '';
+
+    /**
+     * The namespace in which functions are declared. Must not have trailing namespace separator.
+     * @var string
+     */
+    protected $namespace = '';
+
+    /**
+     * Magic method to wrap function calls
+     *
+     * @param string $method
+     * @param array $args
+     */
+    public function __call($method, $args)
+    {
+    }
+
+    /**
+     * Make sure the method is in the array of allowed names
+     *
+     * Prefix is prepended to the method name.
+     *
+     * @param string $method
+     * @throws \Exception if the method name is invalid
+     * @throws \Exception if the function does not exist
+     */
+    protected function validateMethodName($method)
+    {
+    }
+
+    /**
+     * @return array An array of allowed function names
+     */
+    abstract protected function getAllowedMethods();
+}
---
+++ tests/Util/FunctionLibWrapperTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace TC\Services\Scripts\tests\Util;
+
+use PHPUnit_Framework_TestCase;
+
+class FunctionLibWrapperTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testMagicCallMethod()
+    {
+        $this->markTestIncomplete();
+    }
+}
