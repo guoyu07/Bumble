7bab00e228174ffa457f26337564acaa0976f7b7
John P. Bloch <john.bloch@10up.com>
2014-07-26 16:09:21 -0600

    Tests for posix and pcntl wrappers

--- tests/Util/PcntlTest.php
+++ tests/Util/PcntlTest.php
@@ -7,4 +7,45 @@ use PHPUnit_Framework_TestCase;
 class PcntlTest extends PHPUnit_Framework_TestCase
 {
 
+    public function testGetAllowedMethods()
+    {
+        $expected = [
+            '​alarm',
+            '​errno',
+            '​exec',
+            '​fork',
+            '​get_​last_​error',
+            '​getpriority',
+            '​setpriority',
+            '​signal_​dispatch',
+            '​signal',
+            '​sigprocmask',
+            '​sigtimedwait',
+            '​sigwaitinfo',
+            '​strerror',
+            '​wait',
+            '​waitpid',
+            '​wexitstatus',
+            '​wifexited',
+            '​wifsignaled',
+            '​wifstopped',
+            '​wstopsig',
+            '​wtermsig',
+        ];
+        $pcntl = new Pcntl();
+        $getAllowedMethods = new \ReflectionMethod(get_class($pcntl), 'getAllowedMethods');
+        $getAllowedMethods->setAccessible(true);
+        $this->assertEquals($expected, $getAllowedMethods->invoke($pcntl));
+    }
+
+    public function testPcntlPrefixAndNamespace()
+    {
+        $pcntl = new Pcntl();
+        $prefix = new \ReflectionProperty(get_class($pcntl), 'prefix');
+        $prefix->setAccessible(true);
+        $this->assertEquals('pcntl_', $prefix->getValue($pcntl));
+        $namespace = new \ReflectionProperty(get_class($pcntl), 'namespace');
+        $namespace->setAccessible(true);
+        $this->assertEquals('', $namespace->getValue($pcntl));
+    }
 }
--- tests/Util/PosixTest.php
+++ tests/Util/PosixTest.php
@@ -7,4 +7,60 @@ use PHPUnit_Framework_TestCase;
 class PosixTest extends PHPUnit_Framework_TestCase
 {
 
+    public function testGetAllowedMethods()
+    {
+        $expected = [
+            '​access',
+            '​ctermid',
+            '​errno',
+            '​get_​last_​error',
+            '​getcwd',
+            '​getegid',
+            '​geteuid',
+            '​getgid',
+            '​getgrgid',
+            '​getgrnam',
+            '​getgroups',
+            '​getlogin',
+            '​getpgid',
+            '​getpgrp',
+            '​getpid',
+            '​getppid',
+            '​getpwnam',
+            '​getpwuid',
+            '​getrlimit',
+            '​getsid',
+            '​getuid',
+            '​initgroups',
+            '​isatty',
+            '​kill',
+            '​mkfifo',
+            '​mknod',
+            '​setegid',
+            '​seteuid',
+            '​setgid',
+            '​setpgid',
+            '​setsid',
+            '​setuid',
+            '​strerror',
+            '​times',
+            '​ttyname',
+            '​uname',
+        ];
+        $posix = new Posix();
+        $getAllowedMethods = new \ReflectionMethod(__NAMESPACE__ . '\Posix', 'getAllowedMethods');
+        $getAllowedMethods->setAccessible(true);
+        $this->assertEquals($expected, $getAllowedMethods->invoke($posix));
+    }
+
+    public function testPosixPrefixAndNamespace()
+    {
+        $posix = new Posix();
+        $prefix = new \ReflectionProperty(get_class($posix), 'prefix');
+        $prefix->setAccessible(true);
+        $this->assertEquals('posix_', $prefix->getValue($posix));
+        $namespace = new \ReflectionProperty(get_class($posix), 'namespace');
+        $namespace->setAccessible(true);
+        $this->assertEquals('', $namespace->getValue($posix));
+    }
 }
