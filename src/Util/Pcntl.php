d88cc9ff563f47b201b1368416387a950cfec6fd
John P. Bloch <john.bloch@10up.com>
2014-07-26 16:13:04 -0600

    Get pcntl and posix wrappers passing tests

--- src/Util/Pcntl.php
+++ src/Util/Pcntl.php
@@ -5,11 +5,35 @@ namespace League\Bumble\Util;
 class Pcntl extends FunctionLibWrapper
 {
 
+    protected $prefix = 'pcntl_';
+
     /**
      * @return array An array of allowed function names
      */
     protected function getAllowedMethods()
     {
-        // TODO: Implement getAllowedMethods() method.
+        return [
+            'alarm',
+            'errno',
+            'exec',
+            'fork',
+            'get_last_error',
+            'getpriority',
+            'setpriority',
+            'signal_dispatch',
+            'signal',
+            'sigprocmask',
+            'sigtimedwait',
+            'sigwaitinfo',
+            'strerror',
+            'wait',
+            'waitpid',
+            'wexitstatus',
+            'wifexited',
+            'wifsignaled',
+            'wifstopped',
+            'wstopsig',
+            'wtermsig',
+        ];
     }
 }
--- src/Util/Posix.php
+++ src/Util/Posix.php
@@ -5,11 +5,50 @@ namespace League\Bumble\Util;
 class Posix extends FunctionLibWrapper
 {
 
+    protected $prefix = 'posix_';
+
     /**
      * @return array An array of allowed function names
      */
     protected function getAllowedMethods()
     {
-        // TODO: Implement getAllowedMethods() method.
+        return [
+            'access',
+            'ctermid',
+            'errno',
+            'get_last_error',
+            'getcwd',
+            'getegid',
+            'geteuid',
+            'getgid',
+            'getgrgid',
+            'getgrnam',
+            'getgroups',
+            'getlogin',
+            'getpgid',
+            'getpgrp',
+            'getpid',
+            'getppid',
+            'getpwnam',
+            'getpwuid',
+            'getrlimit',
+            'getsid',
+            'getuid',
+            'initgroups',
+            'isatty',
+            'kill',
+            'mkfifo',
+            'mknod',
+            'setegid',
+            'seteuid',
+            'setgid',
+            'setpgid',
+            'setsid',
+            'setuid',
+            'strerror',
+            'times',
+            'ttyname',
+            'uname',
+        ];
     }
 }
